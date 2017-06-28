8b9c4db8093f6ccb82b23d0a011a5507de2d2bcc
crooksecurity <crooksecurity@users.noreply.github.com>
2017-06-28 14:17:48 -0500

    Added noninteractive shell client/service use case
    A non-interactive shell implemented over opendxl.

---
+++ noninteractive_sh_client.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#client
+import logging, os, sys, argparse
+from dxlclient.callbacks import RequestCallback
+from dxlclient.client import DxlClient
+from dxlclient.client_config import DxlClientConfig
+from dxlclient.message import Message, Request, Response
+
+parser = argparse.ArgumentParser(description="Sends command line arguments to any computers listening on the topic with the appropriate service.")
+parser.add_argument("-c", default="ls", dest="command", help="The command to execute on the system running the service")
+args = parser.parse_args()
+if args.command == 0:
+	print "Required paramenter missing.  Please supply -c <command>.  This command will be executed by the remote system"
+	quit()
+else:
+	command = args.command
+
+#Setup Logging
+log_formatter = logging.Formatter('%(asctime)s %(name)s - %(levelname)s - %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+logger = logging.getLogger()
+logger.addHandler(console_handler)
+logger.setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
+
+#[CONFIG OPTIONS]#
+SERVICE_TOPIC = "/studentname/nc/service/nc"
+CONFIG_FILE = "/usr/local/etc/opendxl/dxlclient.config"
+
+config = DxlClientConfig.create_dxl_config_from_file(CONFIG_FILE)
+
+with DxlClient(config) as client:
+    client.connect()
+    # Create the request message
+    req = Request(SERVICE_TOPIC)
+    # Populate the request payload
+    req.payload = command.encode()
+    # Send the request and wait for a response (synchronous)
+    res = client.sync_request(req)
+    # Extract information from the response (if an error did not occur)
+    if res.message_type != Message.MESSAGE_TYPE_ERROR:
+        print "Client received response payload: \n" + res.payload.decode()
---
+++ noninteractive_sh_service.py
@@ -0,0 +1,50 @@
+#Service
+import logging, os, sys, time, subprocess, re
+from dxlclient.callbacks import RequestCallback
+from dxlclient.client import DxlClient
+from dxlclient.client_config import DxlClientConfig
+from dxlclient.message import Message, Request, Response
+from dxlclient.service import ServiceRegistrationInfo
+
+#Setup Logging
+log_formatter = logging.Formatter('%(asctime)s %(name)s - %(levelname)s - %(message)s')
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+logger = logging.getLogger()
+logger.addHandler(console_handler)
+logger.setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
+
+#[CONFIG OPTIONS]#
+SERVICE_TOPIC = "/studentname/nc/service/nc"
+CONFIG_FILE = "/usr/local/etc/opendxl/dxlclient.config"
+
+config = DxlClientConfig.create_dxl_config_from_file(CONFIG_FILE)
+with DxlClient(config) as client:
+	client.connect()
+	class MyRequestCallback(RequestCallback):
+		def on_request(self, request):
+			# Extract information from request
+			print "Service received request payload: " + request.payload.decode()
+			# Create the response message
+			res = Response(request)
+			try:
+				s = request.payload.decode()
+				cmd = subprocess.Popen(re.split(r'\s+', s), stdout=subprocess.PIPE)
+				cmd_out = cmd.stdout.read()
+				# Process output
+				res.payload = cmd_out.encode()
+			except OSError:
+				res.payload = 'Invalid command'.encode()
+			# Send the response
+			client.send_response(res)
+	# Create service registration object
+	info = ServiceRegistrationInfo(client, "myService")
+	# Add a topic for the service to respond to
+	info.add_topic(SERVICE_TOPIC, MyRequestCallback())
+	# Register the service with the fabric (wait up to 10 seconds for registration to complete)
+	client.register_service_sync(info, 10)
+	logger.info("Command Service is running...")
+	while True:
+		time.sleep(60)
